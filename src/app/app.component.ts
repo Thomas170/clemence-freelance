87548156847cfafdf1ba4930947b1cecbb667aaa
Thomas <thomas.cacelles@epita.fr>
2025-08-27 15:59:20 +0200

    hero-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { MyServicesComponent } from "./components/my-services/my-services.component";
+import { AboutMeComponent } from "./components/about-me/about-me.component";
+import { SendMailComponent } from "./components/send-mail/send-mail.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    HeroSectionComponent,
+    MyServicesComponent,
+    AboutMeComponent,
+    SendMailComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
