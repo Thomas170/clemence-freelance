c54509efb3a5281de0f057381b26be4e856ea5bd
Thomas <thomas.cacelles@epita.fr>
2025-10-25 15:39:57 +0200

    seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/shared/header/header.component";
 import { FooterComponent } from "./components/shared/footer/footer.component";
 import { UiToasterComponent } from "./components/shared/ui-toaster/ui-toaster.component";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -16,4 +17,24 @@ import { UiToasterComponent } from "./components/shared/ui-toaster/ui-toaster.co
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private platformId = inject(PLATFORM_ID);
+
+  constructor() {
+    // Charger les polices secondaires après le rendu initial
+    afterNextRender(() => {
+      if (isPlatformBrowser(this.platformId)) {
+        this.loadSecondaryFonts();
+      }
+    });
+  }
+
+  private loadSecondaryFonts() {
+    // Attendre que la page soit interactive
+    setTimeout(() => {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = 'assets/fonts-secondary.css';
+      document.head.appendChild(link);
+    }, 1000); // Délai de 1 seconde après le chargement
+  }
 }
