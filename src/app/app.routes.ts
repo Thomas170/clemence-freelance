204cb320ec080569af8eb6a4151506b93524a5cb
Thomas <thomas.cacelles@epita.fr>
2025-10-25 15:46:53 +0200

    seo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { ServicesComponent } from './components/pages/services/services.componen
 import { HomeComponent } from './components/pages/home/home.component';
 import { ProjectsComponent } from './components/pages/projects/projects.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent },
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'services', component: ServicesComponent },
     { path: 'projects', component: ProjectsComponent },
 ];
